67e54e449a7c7b42a5d90b0eb921173d1697629d
Elon Aseneka <elonsantos63@gmail.com>
2022-12-08 12:09:13 +0300

    login form validation and auth guard

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,10 +5,16 @@ import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { LightboxModule } from 'ngx-lightbox';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const COMPONENTS_FOR_EXPORT = [
   MovieCardComponent,
   NavBarComponent
+]
+
+const MODULES_FOR_EXPORT = [
+  ReactiveFormsModule,
+  FormsModule
 ]
 
 @NgModule({
@@ -19,10 +25,12 @@ const COMPONENTS_FOR_EXPORT = [
     CommonModule,
     RouterModule,
     LightboxModule,
-    HttpClientModule
+    HttpClientModule,
+    ...MODULES_FOR_EXPORT
   ],
   exports: [
-    ...COMPONENTS_FOR_EXPORT
+    ...COMPONENTS_FOR_EXPORT,
+    ...MODULES_FOR_EXPORT
   ]
 })
 export class SharedModule { }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { trigger, transition, useAnimation } from '@angular/animations';
 import { slideInDown } from 'ng-animate';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,21 @@ import { Router } from '@angular/router';
     trigger('slideInDown', [transition('* => *', useAnimation(slideInDown))])
   ],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   slideInDown: any;
+  loginForm!: FormGroup;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   handleLogin() {
     this.router.navigate(['/home'])
