18f58431fafa20319f9b46d5f4d358b9dd204c68
Elon Aseneka <elonsantos63@gmail.com>
2022-12-08 13:44:24 +0300

    commit env files

--- src/app/headers.interceptor.ts
+++ src/app/headers.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable()
 export class HeadersInterceptor implements HttpInterceptor {
@@ -15,8 +16,8 @@ export class HeadersInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     request = request.clone({
       setHeaders: {
-        'X-RapidAPI-Key': 'b4f553c90bmsh592dd8c5885875bp1302a8jsn581b3431b255',
-        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+        'X-RapidAPI-Key': environment.X_RapidAPI_Key,
+        'X-RapidAPI-Host': environment.X_RapidAPI_Host
       }
     })
     return next.handle(request);
